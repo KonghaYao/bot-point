79eb97581b028d93b6579d6b86deb3279f8a359a
3446798488 <3446798488@qq.com>
2025-02-06 09:54:30 +0000

    feat: 翻译器完成

--- server/routes/article/summary.ts
+++ server/routes/article/summary.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 const schema = z.object({
     article: z.string(),
-    // resultLength: z.number().optional().default(200),
+    resultLength: z.number().default(200),
 });
 
 export default defineCompose(
@@ -11,7 +11,7 @@ export default defineCompose(
     defineAI({
         getPrompt(event) {
             const json: z.infer<typeof schema> = useJSON(event)
-            return `请将以下文本总结为${json.resultLength||100}字左右的内容，确保总结简洁明了、易于理解，并且能够抓住原文的主要观点。这段文本将会显示在搜索引擎中，需要很好的 SEO 总结。请根据以下内容回复到 summary 字段：
+            return `请将以下文本总结为${json.resultLength}字左右的内容，确保总结简洁明了、易于理解，并且能够抓住原文的主要观点。这段文本将会显示在搜索引擎中，需要很好的 SEO 总结。请根据以下内容回复到 summary 字段：
             ${json.article}`
         },
 
