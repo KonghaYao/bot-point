ac5789ccaf258e2e0dd6087025e6ebd66dd8c8bd
3446798488 <3446798488@qq.com>
2025-02-07 10:01:50 +0000

    feat: 直接透传 response

--- server/utils/defineAI.ts
+++ server/utils/defineAI.ts
@@ -5,20 +5,29 @@ const openai = new OpenAI({
     apiKey: useRuntimeConfig().NITRO_OPENAI_API_KEY,
     baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1",
 });
-export const defineAI = (opt: {
-    getPrompt: (event: H3Event) => string
-    getSystem?: (event: H3Event) => string
-}): ComposeEventHandler => async (event) => {
-    const { getPrompt, getSystem } = opt;
-    const prompt = getPrompt(event);
-    const system = getSystem?.(event) || '你是一个 985 毕业的高级人才，责任心强，能力高超，你会根据要求以最高标准完成任务，这是你工作的内容。你工作的时候会总结字段并输出 JSON 字符串，不要输出其它无关内容';
-    const completion =await  openai.chat.completions.create({
-        model: "qwen-plus",
-        messages: [
-            { role: "system", content: system },
-            { role: "user", content: prompt }
-        ],
-        response_format: { "type": "json_object" }
-    });
-    return completion.choices[0].message.content
-}
+export const defineAI =
+    (opt: {
+        getPrompt: (event: H3Event) => string;
+        getSystem?: (event: H3Event) => string;
+    }): ComposeEventHandler =>
+    async (event) => {
+        const { getPrompt, getSystem } = opt;
+        const stream = !!getQuery(event).stream;
+        const prompt = getPrompt(event);
+        const system =
+            getSystem?.(event) ||
+            "你是一个 985 毕业的高级人才，责任心强，能力高超，你会根据要求以最高标准完成任务，这是你工作的内容。你工作的时候会总结字段并输出 JSON 字符串，不要输出其它无关内容";
+        const completion = openai.chat.completions.create({
+            model: "qwen-plus",
+            messages: [
+                { role: "system", content: system },
+                { role: "user", content: prompt },
+            ],
+            stream,
+            response_format: stream ? undefined : { type: "json_object" },
+        });
+
+        const res = await completion.asResponse();
+
+        return sendWebResponse(event, res);
+    };
