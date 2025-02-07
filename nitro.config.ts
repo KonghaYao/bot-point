9a3fcfce852d0e3c5b50d88d7730fab6d6ad9058
3446798488 <3446798488@qq.com>
2025-02-07 09:16:42 +0000

    feat: 完成服务端设计

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,4 +3,9 @@ export default defineNitroConfig({
     srcDir: "server",
     compatibilityDate: "2025-02-06",
     preset: "netlify-edge",
+    runtimeConfig: {
+        NITRO_BOTPOINT_TOKEN: process.env.NITRO_BOTPOINT_TOKEN,
+        NITRO_OPENAI_API_BASE_URL: process.env.NITRO_OPENAI_API_BASE_URL,
+        NITRO_OPENAI_API_KEY: process.env.NITRO_OPENAI_API_KEY,
+    },
 });
