7e1b952f7c304cd980e7ff756ef48eba8b9e7378
江夏尧 <3446798488@qq.com>
2025-02-17 19:47:22 +0800

    fix: 改目标为稳定服务器

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,7 +2,7 @@
 export default defineNitroConfig({
     srcDir: "server",
     compatibilityDate: "2025-02-06",
-    preset: "netlify-edge",
+    preset: "netlify",
     runtimeConfig: {
         NITRO_MODEL: process.env.NITRO_MODEL,
         NITRO_BOTPOINT_TOKEN: process.env.NITRO_BOTPOINT_TOKEN,
